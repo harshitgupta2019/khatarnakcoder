6fde9aa567f501ab8faff23e5c3bb3505b2d4ab6
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2020-04-27 13:34:45 +0530

    Update edit distance.cpp

--- DSA/week5/edit distance.cpp
+++ DSA/week5/edit distance.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 int f(string s1,string s2,int n,int m)
 {
-	int dp[n+1][m+1];
-	if(n==0)
-	return m;
-	if(m==0)
-	return n;
-	if(s1[n-1]==s2[m-1])
-	return dp[n][m]=f(s1,s2,n-1,m-1);
-	else
-	{
-		return 1+min(f(s1,s2,n-1,m),min(f(s1,s2,n,m-1),f(s1,s2,n-1,m-1)));
-	}
-	
-}
+	int dp[n+1][m+1],i,j;
+	for ( i = 0; i <= n; i++) 
+	{ 
+        for ( j = 0; j <= m; j++) 
+        { 
+            if (i == 0) 
+                dp[i][j] = j;
+            else if (j == 0) 
+                 dp[i][j]=i;
+            else if (s1[i - 1] == s2[j - 1]) 
+                dp[i][j] = dp[i - 1][j - 1]; 
+            else
+                dp[i][j] = 1 + min(dp[i][j - 1],min(dp[i - 1][j], dp[i - 1][j - 1]));
+        } 
+    } 
+    return dp[n][m]; 
+} 
 int main() {
 	string s1,s2;
 	cin>>s1>>s2;
