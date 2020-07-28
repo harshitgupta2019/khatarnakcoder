8c42cb1452a57bd191ef434e448043d92636525a
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2020-07-28 17:38:34 +0530

    Create 962D-Merge Equals.cpp

---
+++ codeforces/962D-Merge Equals.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	long long int n;
+	map<long long int,long long int>m;
+	cin>>n;
+	long long int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	for(int i=0;i<n;i++)
+	{
+		if(m[a[i]])
+		{
+		  a[m[a[i]]]=-1;
+		  m[a[i]]=0;
+		  a[i]*=2;
+		  i--;
+		}
+		else
+		m[a[i]]=i;
+	}
+	long long int c=0;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]!=-1)
+		c++;
+	}
+	cout<<c<<endl;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]!=-1)
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
