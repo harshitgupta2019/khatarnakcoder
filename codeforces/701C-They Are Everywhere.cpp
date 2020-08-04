49ebc699670b41b046570a676137087f04cb1d5f
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2020-08-04 12:03:08 +0530

    Update 701C-They Are Everywhere.cpp

--- codeforces/701C-They Are Everywhere.cpp
+++ codeforces/701C-They Are Everywhere.cpp
@@ -1,53 +1,40 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define inf 0x3f3f3f3f
 using namespace std;
 
-#define st first
-#define nd second
-#define mp make_pair
-#define pb push_back
-#define cl(x, v) memset((x), (v), sizeof(x))
+map<int,int> st; 
 
-#define db(x) cerr << #x << " == " << x << endl
-#define dbs(x) cerr << x << endl
-#define _ << ", " <<
-
-typedef long long ll;
-typedef long double ld;
-
-typedef pair<int, int> pii;
-typedef pair<int, pii> piii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-
-const ld EPS = 1e-9, PI = acos(-1.);
-const int INF = 0x3f3f3f3f, MOD = 1e9+7;
-const int N = 1e5+5;
-
-int n, c[300], t;
-char s[N];
-
-int main() 
-{
-  scanf("%d %s", &n, s);
-
-  for (int i = 0; s[i]; ++i)
-  if (!c[s[i]]) 
-  c[s[i]]++, t++;
-  for (int i = 0; i < 300; ++i) 
-  c[i]=0;
-  int ans = INF, cnt;
-  int l = 0, r = 0;
-  while (r < n) 
-  {
-    if (!c[s[r]]) cnt++;
-    c[s[r]]++;
-    r++;
-    while (l < r and c[s[l]]>1) 
-    c[s[l]]--, l++;
-    if (cnt == t)
-    ans = min(ans, r-l);
-  }
-  printf("%d\n", ans);
-
-  return 0;
+int a[100001];
+int main(){
+	int n;
+	cin>>n;
+	char c;
+	set<int> s;
+	for(int i=0;i<n;i++)
+	{
+		cin>>c;
+	    a[i] = c-'A'+1;
+		s.insert(a[i]);
+	}
+	int cnt = s.size();
+	s.clear();
+	
+	int s1 = 0,t=0,sum=0;
+	int res = inf;
+	while(1)
+	{
+	   while(sum < cnt && t < n)
+	   {
+	   	if(st[a[t++]]++==0)
+	   		sum++;
+	   }
+	   if(sum<cnt)
+	   break;
+	   res = min(res,t-s1);
+	   if(--st[a[s1++]]==0)
+	   	sum--;
+	   	cout<<sum;
+	}
+	cout<<res<<endl;
+	return 0;
 }
